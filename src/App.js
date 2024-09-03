7b2602990eb500dbb2b63361445aeeefe2497c3a
YenyVillalba <yeny_0192@hotmail.com>
2024-09-03 18:29:58 -0500

    inicializacion
    inicio de proyecto react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from "react";
+
+function Saludo() {
+  return (
+    <div className='Saludo'>
+      <h2> Hola,  </h2>
+      <p>mi nombre es Yeny Vilalba</p>
+    <h1>Pertenezco a Mujeres Digitales</h1>
+    </div>
+  );
+}
+
+export default Saludo;
